5ab17ed1647eb95d529fdfe27e8e6da811917cd6
fran-man <fran.man@hotmail.co.uk>
2013-01-20 17:18:01 +0000

    Added code to parse format from URL and check against pre-defined valid values

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var Server = express();
 
-Server.get(/.*/,function(req,res){
-	res.send('it works!!');
+Server.get(/\/generate/,function(req,res){
+	var responseContents = '';
+	
+	/* Pull parameters out of URL */
+	var dataType = req.query['format'];
+	dataType = dataType.toUpperCase();
+	
+	/* Currently only works for JSON,
+	 * Check now... */
+	if(dataType != 'JSON'){
+		responseContents = "Invalid format, please visit <not yet defined> for usage";
+		res.header('Content-Type','text/plain');
+		res.send(responseContents);
+	}
+	else{
+		res.header('Content-Type','text/plain');
+		res.send('Format:' + dataType);
+	}
 });
 
 Server.listen('9999');
